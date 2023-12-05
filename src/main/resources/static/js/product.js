f64bcb7fb8a4b502cdfbfe8e930a161c7000dca9
guojunf <guojunf@yonyou.com>
2023-12-05 10:40:29 +0800

    add static folder

---
+++ src/main/resources/static/js/product.js
@@ -0,0 +1,51 @@
+$(document).ready(function() {
+	$('#loader').hide();
+	$("#submit").on("click", function() {
+		$("#submit").prop("disabled", true);
+		var file = $("#image").val();
+		var data = new FormData($("#form")[0]);
+		$('#loader').show();
+		if (file === "") {
+			$("#submit").prop("disabled", false);
+			$('#loader').hide();
+			$("#image").css("border-color", "red");
+			$("#error_name").html("Please fill the required field.");
+			$("#error_file").html("Please fill the required field.");
+			$("#error_price").html("Please fill the required field.");
+			$("#error_description").html("Please fill the required field.");
+		} else {
+			$("#image").css("border-color", "");
+			$('#error_name').css('opacity', 0);
+			$('#error_file').css('opacity', 0);
+			$('#error_price').css('opacity', 0);
+			$('#error_description').css('opacity', 0);
+			$.ajax({
+				type: 'POST',
+				enctype: 'multipart/form-data',
+				data: data,
+				url: "/photos/image/upload",
+				processData: false,
+				contentType: false,
+				cache: false,
+				success: function(data, statusText, xhr) {
+					console.log(xhr.status);
+					if (xhr.status == "200") {
+						$('#loader').hide();
+						$("#form")[0].reset();
+						$('#success').css('display', 'block');
+						$("#error").text("");
+						$("#success").html("Product Inserted Succsessfully.");
+						$('#success').delay(2000).fadeOut('slow');
+					}
+				},
+				error: function(e) {
+					$('#loader').hide();
+					$('#error').css('display', 'block');
+					$("#error").html("Oops! something went wrong.");
+					$('#error').delay(5000).fadeOut('slow');
+					location.reload();
+				}
+			});
+		}
+	});
+});
